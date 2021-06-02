512337856cc7af6af2db3b6a1b91fe2f84057a82
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-02 14:14:20 -0700

    Adds logic to check for OS theme preference settings

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = () => {
-  const [value, setValue] = useLocalStorage("dark");
-  return [value, setValue];
+  const [enabledState, setEnabledState] = useLocalStorage("dark");
+  const prefersDarkMode = usePrefersDarkMode();
+  const enabled =
+    typeof enabledState !== "undefined" ? enabledState : prefersDarkMode;
+
+  return [enabled, setEnabledState];
+};
+
+const useMedia = (queries, values, defaultValue) => {
+  const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+  const getValue = () => {
+    const index = mediaQueryLists.findIndex((mql) => mql.matches);
+    return typeof values[index] !== "undefined" ? values[index] : defaultValue;
+  };
+  return getValue();
+};
+
+const usePrefersDarkMode = () => {
+  return useMedia(["(prefers-color-scheme: dark)"], [true], false);
 };
 
 export default useDarkMode;
